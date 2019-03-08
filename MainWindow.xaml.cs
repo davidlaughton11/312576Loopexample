9e9c03777496f5b9299ad2a70e8ce05eb72eff2c
davidlaughton11 <47399940+davidlaughton11@users.noreply.github.com>
2019-03-08 14:40:23 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿/* David Laughton
+ * march 8 2019
+ * While examples
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _312576WhileExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Make sure to add a text file to your project
+            //set the build to copy
+            System.IO.StreamReader reader= new System.IO.StreamReader("myFile.txt");
+            try {
+                String output = "";
+                while (!reader.EndOfStream)
+                {
+                    output += reader.ReadLine() + Environment.NewLine;
+                }
+                output = output.Trim();
+                lblOutput.Content = output;
+            } catch (Exception ex) { }
+        }
+    }
+}
